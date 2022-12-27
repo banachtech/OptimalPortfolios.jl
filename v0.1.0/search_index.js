77b247829f2428f0f38c8841f0ddc17e90adfe16
Documenter.jl <documenter@juliadocs.github.io>
2022-12-27 17:58:07 +0000

    build based on b1385e0

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#OptimalPortfolios.jl","page":"Index","title":"OptimalPortfolios.jl","text":"","category":"section"},{"location":"","page":"Index","title":"Index","text":"Documentation for OptimalPortfolios.jl","category":"page"},{"location":"","page":"Index","title":"Index","text":"Package repository.","category":"page"}]
+}
